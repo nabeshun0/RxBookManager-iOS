1accd8777b0722b6c013393c1187f73474c5e6ec
snabezawa-cq <s.nabezawa@caraquri.com>
2019-06-26 20:07:06 +0900

    first commit

---
+++ RxBookManager-iOS/RxBookManager-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxBookManager-iOS
+//
+//  Created by member on 2019/06/26.
+//  Copyright © 2019 nabezawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
