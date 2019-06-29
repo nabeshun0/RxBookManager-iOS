3d6c94b4afd77f82d51a2307a8283c9f6f24af21
snabezawa-cq <s.nabezawa@caraquri.com>
2019-06-29 14:40:02 +0900

    MainTabファイル作成

---
+++ RxBookManager-iOS/Presentation/MainTab/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  RxBookManager-iOS
+//
+//  Created by member on 2019/06/29.
+//  Copyright © 2019 nabezawa. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
