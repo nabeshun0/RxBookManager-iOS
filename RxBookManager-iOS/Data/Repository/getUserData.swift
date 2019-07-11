ae1ecf285214a870f2f2b322074f5160c549517e
snabezawa-cq <s.nabezawa@caraquri.com>
2019-07-11 11:38:27 +0900

    getUserData,getBookData.swiftを追加
    役割: APIを投げる

---
+++ RxBookManager-iOS/Data/Repository/getUserData.swift
@@ -0,0 +1,28 @@
+import APIKit
+import Result
+import RxSwift
+
+//==================================================
+// MARK: - ログイン、リクエスト・レスポンス
+//==================================================
+protocol GetLoginDataStore {
+    func getLoginResponse(login: LoginModel) -> Single<Result<LoginAPI.Response, SessionTaskError>>
+}
+
+struct GetLoginDataStoreImpl: GetLoginDataStore {
+    func getLoginResponse(login: LoginModel) -> Single<Result<LoginAPI.Response, SessionTaskError>> {
+        let request = LoginAPI.Request(loginModel: login)
+        return Session.rx_sendRequest(request: request)
+    }
+}
+
+//==================================================
+// MARK: - 新規登録、リクエスト・レスポンス
+//==================================================
+
+
+//==================================================
+// MARK: - ログアウト、リクエスト・レスポンス
+//==================================================
+
+
