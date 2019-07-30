0c6191dec74ce1495bfa466d695e734e7777a2f3
snabezawa-cq <s.nabezawa@caraquri.com>
2019-07-30 11:37:45 +0900

    APIレスポンステスト成功
    空文字の場合、FaliureになるがloginModelにemailとpasswordを入力すればSuccessで返ってくる。

--- RxBookManager-iOSTests/Data/DataStore/getLoginDataStoreTests.swift
+++ RxBookManager-iOSTests/Data/DataStore/getLoginDataStoreTests.swift
@@ -19,15 +19,18 @@ class getLoginDataStoreTests: XCTestCase {
         let dataStore = GetLoginDataStoreImpl()
         let login = LoginModel(email: "", password: "")
         let disposebag = DisposeBag()
-
+        let exp = expectation(description: "loginRequest")
 
         dataStore.getLoginResponse(login: login)
             .subscribe(onSuccess: { response in
                 print(response)
-
+                exp.fulfill()
             }, onError: { error in
                 print(error)
+                XCTAssert(false)
             }).disposed(by: disposebag)
+
+        wait(for: [exp], timeout: 5.0)
     }
 
 
