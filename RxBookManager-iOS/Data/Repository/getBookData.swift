1b9da492d42f0adb2188aa135361d14571f0575d
snabezawa-cq <s.nabezawa@caraquri.com>
2019-07-30 19:36:40 +0900

    書籍一覧モデルを作成
    モデル、レスポンス、リクエスト

--- RxBookManager-iOS/Data/Repository/getBookData.swift
+++ RxBookManager-iOS/Data/Repository/getBookData.swift
@@ -6,6 +6,16 @@ import RxSwift
 // MARK: - 書籍一覧、リクエスト・レスポンス
 //==================================================
 
+protocol FetchBookListDataStore {
+    func fetchBookListResponse(fetchBookList: FetchBookListModel) -> Single<FetchBookListAPI.Response>
+}
+
+struct FetchBookListDataStoreImpl: FetchBookListDataStore {
+    func fetchBookListResponse(fetchBookList: FetchBookListModel) -> Single<FetchBookListAPI.Response> {
+        let request = FetchBookListAPI.Request(fetchBookListModel: fetchBookList)
+        return Session.rx_sendRequest(request: request)
+    }
+}
 
 //==================================================
 // MARK: - 書籍追加、リクエスト・レスポンス
