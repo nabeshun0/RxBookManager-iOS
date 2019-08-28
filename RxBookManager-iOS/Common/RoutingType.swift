b60ec77f7401464b5fe255eba778c080aadd6378
ikawashima-cq <i.kawashima@caraquri.com>
2019-08-29 00:44:23 +0900

    create Routingtypes

---
+++ RxBookManager-iOS/Common/RoutingType.swift
@@ -0,0 +1,14 @@
+//
+//  RoutingType.swift
+//  RxBookManager-iOS
+//
+//  Created by Iichiro Kawashima on 2019/08/29.
+//  Copyright © 2019 nabezawa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol RoutingType {
+    var viewcontroller: UIViewController? { get }
+}
