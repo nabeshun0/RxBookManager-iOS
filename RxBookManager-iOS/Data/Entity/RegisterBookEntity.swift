8a84963be8640e593fc7b4f1cd65d08a4962b051
ikawashima-cq <i.kawashima@caraquri.com>
2019-08-29 22:08:47 +0900

    modify Entity

--- RxBookManager-iOS/Data/Entity/RegisterBookEntity.swift
+++ RxBookManager-iOS/Data/Entity/RegisterBookEntity.swift
@@ -7,21 +7,5 @@ extension RegisterBookAPI {
     struct Response: Decodable {
         let status: Int
         let result: Book
-
-        struct Book: Decodable {
-            var id: Int
-            var name: String
-            var image: String?
-            var price: Int?
-            var purchaseDate: String?
-
-            enum CodingKeys: String, CodingKey {
-                case purchaseDate = "purchase_date"
-                case id
-                case name
-                case image
-                case price
-            }
-        }
     }
 }
