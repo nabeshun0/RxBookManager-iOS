005cafbaddb503e623fe6898adff41488baeeb8c
ikawashima-cq <i.kawashima@caraquri.com>
2019-08-29 12:47:35 +0900

    fix indent

--- RxBookManager-iOS/Data/APIRoutes.swift
+++ RxBookManager-iOS/Data/APIRoutes.swift
@@ -11,12 +11,12 @@ enum APIRoutes {
 
     func configurePath() -> (method: HTTPMethod, path: String) {
         switch self {
-        case .login: return (.post, "/login")
-        case .signUp: return (.post, "/sign_up")
-        case .logout: return (.delete, "/logout")
-        case .fetchBookList: return (.get, "/books")
-        case .registerBook: return (.post, "/books")
-        case .detailBook: return (.put, "/books")
+        case .login:           return (.post, "/login")
+        case .signUp:          return (.post, "/sign_up")
+        case .logout:          return (.delete, "/logout")
+        case .fetchBookList:   return (.get, "/books")
+        case .registerBook:    return (.post, "/books")
+        case .detailBook:      return (.put, "/books")
         }
     }
 }
