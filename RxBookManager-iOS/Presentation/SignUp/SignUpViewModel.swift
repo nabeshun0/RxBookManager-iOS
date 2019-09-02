716b15bc70b92ad4b2e1a544aa243bbd69cc7f4d
snabezawa-cq <s.nabezawa@caraquri.com>
2019-09-02 20:59:32 +0900

    SignUpViewModelを実装

--- RxBookManager-iOS/Presentation/SignUp/SignUpViewModel.swift
+++ RxBookManager-iOS/Presentation/SignUp/SignUpViewModel.swift
@@ -1,17 +1,7 @@
-//
-//  SignUpViewModel.swift
-//  RxBookManager-iOS
-//
-//  Created by member on 2019/06/29.
-//  Copyright © 2019 nabezawa. All rights reserved.
-//
-
 import RxSwift
 import APIKit
 
 final class SignUpViewModel {
-
-    private let disposeBag = DisposeBag()
 
     private let dependency: AccountRepository
 
@@ -30,6 +20,36 @@ extension SignUpViewModel {
     }
 
     struct OutPut {
+        let result: Observable<SignUpAPI.Response>
+        let error: Observable<Error>
+        let isValid: Observable<Bool>
+    }
 
+    func transform(input: Input) -> OutPut {
+        let emailTextRelay = input.emailText.map {
+            $0.count >= 6
+        }
+
+        let passwordTextRelay = input.passwordText.map {
+            $0.count >= 6
+        }
+
+        let confirmTextRelay = input.confirmText.map {
+            $0.count >= 6
+        }
+
+        let isValid = Observable.combineLatest(emailTextRelay, passwordTextRelay, confirmTextRelay) { $0 && $1 && $2 }
+
+        let parameter = Observable.combineLatest(input.emailText, input.passwordText)
+
+        let response = input.didSaveButtonTapped
+            .withLatestFrom(parameter)
+            .flatMap { param -> Observable<Event<SignUpAPI.Response>> in
+                let signUpModel = SignUpModel(email: param.0, password: param.1)
+                return self.dependency.signup(signUpModel)
+                .materialize()
+        }.share(replay: 1)
+
+        return OutPut(result: response.elements(), error: response.errors(), isValid: isValid)
     }
 }
