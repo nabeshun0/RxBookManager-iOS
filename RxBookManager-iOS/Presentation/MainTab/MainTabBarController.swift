38156495e4764c950d55d712dc5cf2dd571ce7de
snabezawa-cq <s.nabezawa@caraquri.com>
2019-09-02 21:03:59 +0900

    MainTabBarControllerを実装

--- RxBookManager-iOS/Presentation/MainTab/MainTabBarController.swift
+++ RxBookManager-iOS/Presentation/MainTab/MainTabBarController.swift
@@ -12,13 +12,20 @@ final class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let bookListView = BookListViewController()
-        bookListView.tabBarItem = UITabBarItem(title: "一覧", image: nil, tag: 1)
-        let bookListBar = UINavigationController(rootViewController: bookListView)
-
+        let bookListBar = BookListViewControllerFactory.createInstance()
         let logoutBar = LogoutViewControllerFactory.createInstance()
         setViewControllers([bookListBar, logoutBar], animated: false)
+    }
+}
+
+struct BookListViewControllerFactory {
+    static func createInstance() -> UINavigationController {
+        let dependency = BookRepositoryImpl.shared
+        let viewModel = BookListViewModel(dependency: dependency)
+        let vc = BookListViewController(viewModel: viewModel)
+        vc.tabBarItem = UITabBarItem(title: "一覧", image: #imageLiteral(resourceName: "bookIconTab"), tag: 1)
+        let bookListBar = UINavigationController(rootViewController: vc)
+        return bookListBar
     }
 }
 
@@ -27,7 +34,7 @@ struct LogoutViewControllerFactory {
         let dependency = AccountRepositoryImpl.shared
         let viewModel = LogoutViewModel(dependency: dependency)
         let vc = LogoutViewController(viewModel: viewModel)
-        vc.tabBarItem = UITabBarItem(title: "設定", image: nil, tag: 2)
+        vc.tabBarItem = UITabBarItem(title: "設定", image: #imageLiteral(resourceName: "settingIconTab"), tag: 2)
         let logoutBar = UINavigationController(rootViewController: vc)
         return logoutBar
     }
