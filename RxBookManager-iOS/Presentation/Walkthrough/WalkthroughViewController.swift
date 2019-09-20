8ff457abb4f49aaa52c970e3d74a8c9ca71c64a2
snabezawa-cq <s.nabezawa@caraquri.com>
2019-09-20 10:43:30 +0900

    scrollViewを追加

--- RxBookManager-iOS/Presentation/Walkthrough/WalkthroughViewController.swift
+++ RxBookManager-iOS/Presentation/Walkthrough/WalkthroughViewController.swift
@@ -2,13 +2,27 @@ import UIKit
 
 class WalkthroughViewController: UIViewController {
 
-    var scrollView: UIScrollView!
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
     }
 }
 
 extension WalkthroughViewController {
+
+    private func setupUI() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
     
 }
