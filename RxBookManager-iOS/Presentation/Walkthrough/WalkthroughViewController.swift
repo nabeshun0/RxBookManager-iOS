463a5a3fedf7120c653cf177fb28f3454be682b6
snabezawa-cq <s.nabezawa@caraquri.com>
2019-09-20 10:50:37 +0900

    stackViewを追加

--- RxBookManager-iOS/Presentation/Walkthrough/WalkthroughViewController.swift
+++ RxBookManager-iOS/Presentation/Walkthrough/WalkthroughViewController.swift
@@ -2,10 +2,27 @@ import UIKit
 
 class WalkthroughViewController: UIViewController {
 
+    //==================================================
+    // MARK: - Presentation
+    //==================================================
+
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         return scrollView
     }()
+
+    private let stacKView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
+
+    //==================================================
+    // MARK: - Routing
+    //==================================================
+
+    //==================================================
+    // MARK: - UIViewController override
+    //==================================================
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +40,14 @@ extension WalkthroughViewController {
         scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
         scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
+        scrollView.addSubview(stacKView)
+        stacKView.translatesAutoresizingMaskIntoConstraints = false
+        stacKView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        stacKView.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
+        stacKView.rightAnchor.constraint(equalTo: scrollView.rightAnchor).isActive = true
+        stacKView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+
     }
     
 }
