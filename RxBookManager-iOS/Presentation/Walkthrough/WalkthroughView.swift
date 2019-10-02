609a3a6f69e76506935638a201272d91737190e6
snabezawa-cq <s.nabezawa@caraquri.com>
2019-10-02 21:44:50 +0900

    pageControl等、サブビューを制約で位置付け

--- RxBookManager-iOS/Presentation/Walkthrough/WalkthroughView.swift
+++ RxBookManager-iOS/Presentation/Walkthrough/WalkthroughView.swift
@@ -43,6 +43,13 @@ extension WalkthroughView {
 
 final class WalkthroughView: UIView {
 
+    let descriptionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Screen Shot 2019-05-20 at 20.12.36")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
     let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "fugafuga"
@@ -52,15 +59,21 @@ final class WalkthroughView: UIView {
     let descriptionTextView: UITextView = {
         let textView = UITextView()
         textView.text = "hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge"
+        textView.backgroundColor = .clear
+        textView.font = UIFont.systemFont(ofSize: 16)
         return textView
     }()
 
-    let descriptionImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "bookIcon")
-        imageView.contentMode = .scaleAspectFit
-        return imageView
+    let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.backgroundColor = .lightGray
+        pageControl.currentPageIndicatorTintColor = .black
+        pageControl.numberOfPages = 3
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = .white
+        return pageControl
     }()
+
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -73,10 +86,34 @@ final class WalkthroughView: UIView {
 
     func setupUI() {
         self.backgroundColor = .lightGray
+        self.addSubview(descriptionImageView)
         self.addSubview(titleLabel)
+        self.addSubview(descriptionTextView)
+        self.addSubview(pageControl)
+
+        descriptionImageView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        descriptionImageView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        descriptionImageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        descriptionImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 80).isActive = true
+
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 40).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: descriptionImageView.bottomAnchor, constant: 30).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+
+        descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30).isActive = true
+        descriptionTextView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        descriptionTextView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        descriptionTextView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: 30).isActive = true
+        
+        pageControl.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        pageControl.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        pageControl.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
     }
 
     func applyUI(view: WalkthroughView.Layout) {
